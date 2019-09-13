63a90a8970444bf4c02400559f0ba52484f5b172
Edgar Pek <edgar.pek@gmail.com>
2019-09-13 12:25:24 -0500

    minor syntactic fixes

--- P1/int_sqrt.py
+++ P1/int_sqrt.py
@@ -9,8 +9,8 @@ def sqrt(number):
     pass
 
 
-print ("Pass" if  (3 == sqrt(9)) else "Fail")
-print ("Pass" if  (0 == sqrt(0)) else "Fail")
-print ("Pass" if  (4 == sqrt(16)) else "Fail")
-print ("Pass" if  (1 == sqrt(1)) else "Fail")
-print ("Pass" if  (5 == sqrt(27)) else "Fail")
+print("Pass" if (3 == sqrt(9)) else "Fail")
+print("Pass" if (0 == sqrt(0)) else "Fail")
+print("Pass" if (4 == sqrt(16)) else "Fail")
+print("Pass" if (1 == sqrt(1)) else "Fail")
+print("Pass" if (5 == sqrt(27)) else "Fail")
