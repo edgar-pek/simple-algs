51329b80bdce3cd26379f607d0686204409afd6f
Edgar Pek <edgar.pek@gmail.com>
2019-12-26 16:14:40 -0600

    square root based on binary search

--- P1/int_sqrt.py
+++ P1/int_sqrt.py
@@ -1,3 +1,4 @@
+
 def sqrt(number):
     """
     Calculate the floored square root of a number
@@ -5,9 +6,44 @@ def sqrt(number):
         number(int): Number to find floored squared root of
     Returns:
         int: Floored Square Root
+        special case: number < 0 or number is not an integer returns -1
     """
-    pass
+    if not isinstance(number, int):
+        # print("Expected non-negative integer as input")
+        return -1
+    if number < 0:
+        # print("Expecting non-negative integer")
+        return -1
+    if number < 2:
+        return number
+    lo = 2
+    hi = (number >> 5) + 8  # choice of hi will be explained in the text file
+    while lo <= hi:
+        mid = lo + ((hi - lo) >> 1)
+        if mid * mid > number:
+            hi = mid - 1
+        else:
+            lo = mid + 1
+    assert lo == (hi + 1)
+    return lo - 1
 
+
+# added corner cases
+assert sqrt(-1) == -1
+print(sqrt(-1))
+# expected output: -1
+assert sqrt(None) == -1
+print(sqrt(-1))
+# expected output: -1
+assert sqrt([]) == -1
+print(sqrt(-1))
+# expected output: -1
+assert sqrt(float("inf")) == -1
+print(sqrt(-1))
+# expected output: -1
+assert sqrt(256) == 16
+print(sqrt(256))
+# expected output: 16
 
 print("Pass" if (3 == sqrt(9)) else "Fail")
 print("Pass" if (0 == sqrt(0)) else "Fail")
